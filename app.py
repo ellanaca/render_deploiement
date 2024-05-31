a6c19cda6d142591577e00b5cf4a49f9e5b1f89e
ellanaca <107549603+ellanaca@users.noreply.github.com>
2024-05-31 11:44:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,8 @@ import pandas as pd
 import joblib
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
-
 
 load_dotenv()
-
-app = FastAPI()
 
 # Définition de l'URL de l'API FastAPI
 url = os.environ['FASTAPI_URL']
